d5b819421fddaface0c767a74cfa3bfa4150ea4c
Kurt Schwartz <104395023+KurtSchwartz@users.noreply.github.com>
2025-11-03 01:08:41 +0300

    Initial commit: API Yatube backend

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -3,12 +3,13 @@ from posts.models import Post, Group
 from django.shortcuts import get_object_or_404
 from .permissions import IsOwnerOrReadOnly
 from .serializers import PostSerializer, GroupSerializer, CommentSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (IsOwnerOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -22,7 +23,7 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (IsOwnerOrReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
 
     def get_queryset(self):
         post_id = self.kwargs.get('post_id')
