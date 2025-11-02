0200c0e7f5a7f0c27cd16d95aac9746906381558
Kurt Schwartz <104395023+KurtSchwartz@users.noreply.github.com>
2025-11-03 00:51:47 +0300

    Initial commit: API Yatube backend

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -5,6 +5,7 @@ from rest_framework import permissions
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
 
+
     def has_object_permission(self, request, view, obj):
         # Разрешает доступ для GET, HEAD или OPTIONS (SAFE_METHODS)
         if request.method in permissions.SAFE_METHODS:
